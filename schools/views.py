21cb9838f2a24d7b8d19bd0c603010244665e1d4
Thuhaa <josephthuha@gmail.com>
2022-02-05 13:12:55 +0300

    More Leaflet Functionality

--- schools/views.py
+++ schools/views.py
@@ -7,7 +7,12 @@ from .utils import get_geojson
 
 def homepage_view(request):
 	data = json.dumps(get_geojson())
-	context = {"data":data}
+	obj = json.loads(data)
+	data_list = obj['features']
+	'''for x in data_list:
+					institute_list.append(x['properties']['institute'])'''
+
+	context = {"data":data, "data_list":data_list}
 	return render(request, "schools/index.html", context)
 
 def geojson_view(request):
